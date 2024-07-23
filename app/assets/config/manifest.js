daf02be0617a3f6b68302eb64c4b8a895a5d04c5
Paa Yaw <paayaw.dev@gmail.com>
2024-07-23 05:15:13 +0000

    add css and js to asset pipeline for basic styling

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+// including assets from lib/assets directory
+//= link_directory ../../../lib/assets/javascripts .js
+//= link_directory ../../../lib/assets/stylesheets .css
